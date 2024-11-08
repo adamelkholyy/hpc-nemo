95694f631fbd2d633a2ebb1c3eb01c6c38ac0a6a
El Kholy <ae553@login02.cluster.local>
2024-11-08 12:38:37 +0000

    Added sbatch script

---
+++ diarize_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --export=ALL       # export all environment variables to the batch job.
+#SBATCH --partition gpu       # submit to the gpu queue
+#SBATCH -D . # set working directory to . # research project to submit under
+#SBATCH -A Research_Project-T116269
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=a.el-kholy@exeter.ac.uk # email address
+#SBATCH --time=03:00:00    # Maximum wall time for the job.
+#SBATCH --account Research_Project-T116269    # research project to submit under. 
+#SBATCH --nodes=1                                  # specify number of nodes.
+#SBATCH --ntasks-per-node=16        # specify number of processors per node
+#SBATCH --mem-per-cpu=1024         # MB memory requested per cpu-core (task)
+#SBATCH --output=diarize_test.out   # submit script's standard-out
+#SBATCH --error=diarize_test.err    # submit script's standard-error
+#SBATCH --job-name=diarize_test
+
+## print start date and time
+echo ==================================================================================
+echo Job started on:
+date -u
+echo ==================================================================================
+echo -n "This script is running on "
+Hostname
+echo ==================================================================================
+
+## run script
+module load Anaconda3/2023.07-2
+cd /lustre/projects/Research_Project-T116269/nemo
+
+conda activate nemo
+python diarize.py -a audio/AutHERTS01.mp3 --no-stem --whisper-model large-v3 --language English
+
+echo ==================================================================================
+## print end date and time
+echo Job ended on:
+date -u
+echo ==================================================================================
+
